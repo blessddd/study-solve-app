6cef370383aecf13248b70fa644522de53fab998
blessddd <kevinezequielalvareznunez@gmail.com>
2024-06-03 01:18:28 -0300

    feat: Implementación del frontend de la página donde las personas registrarán sus cuentas

--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,27 @@
 "use strict"
 
-const passwordInput = document.querySelector(".password-input");
-const togglePasswordInput = document.querySelector(".form__toggle-container__checkbox");
+const inputContainers = document.querySelectorAll(".login__form__input-container");
+const passwordInput = document.querySelectorAll(".password-input");
+const toggleInputContainer = document.querySelectorAll(".form__toggle-container__checkbox");
 
-togglePasswordInput.addEventListener("click", () => {
-    (passwordInput.getAttribute("type") == "password" ?
-        passwordInput.setAttribute("type", "text") :
-        passwordInput.setAttribute("type", "password")
+toggleInputContainer.forEach(input => 
+    input.addEventListener("click", () => 
+        passwordInput.forEach(input => 
+            input.getAttribute("type") == "password" ?
+            input.setAttribute("type", "text") :
+            input.setAttribute("type", "password")
+        )
     )
-});
+);
 
-document.querySelectorAll('.login__form__input-container').forEach(container => {
-    const input = container.querySelector('.login__form__input');
-    const inputMessage = container.querySelector('.login__form__input-message');
+inputContainers.forEach(container => {
+    const input = container.querySelector(".login__form__input");
+    const inputMessage = container.querySelector(".login__form__input-message");
 
     input.addEventListener('focus', () => {
         // La clase "focused" se usa para desactivar / activar la animación del input cuando el usuario deja de usar el input dependiendo si el input contiene o no información
         container.classList.add('focused');
-        input.style.margin = "5px 0px 0px 0px";
+        input.style.margin = "2px 0px 0px 0px";
     });
 
     // Evento para detectar si el usuario deja de usar el input
@@ -25,7 +29,7 @@ document.querySelectorAll('.login__form__input-container').forEach(container => 
         if(input.value === '') {
             container.classList.remove('focused');
             input.style.margin = "0px";
-            inputMessage.style.margin = "15px 0px 0px 18px";
+            inputMessage.style.margin = "15px 0px 0px 20px";
         }
     });
 
